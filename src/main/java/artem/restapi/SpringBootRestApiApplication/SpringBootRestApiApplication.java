80adf4f7de81b6115705162b5791cb5cc4e9bb7d
artem <pocoone28@gmail.com>
2022-10-27 22:21:58 +0300

    1.0

--- src/main/java/artem/restapi/SpringBootRestApiApplication/SpringBootRestApiApplication.java
+++ src/main/java/artem/restapi/SpringBootRestApiApplication/SpringBootRestApiApplication.java
@@ -2,10 +2,10 @@ package artem.restapi.SpringBootRestApiApplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class SpringBootRestApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootRestApiApplication.class, args);
 	}
